de543bb5f9da13c570d1fb77838b3bddc6734294
lavanya-sethuraman <lavanya.sethuraman23@gmail.com>
2017-03-27 11:21:20 -0400

    Style app

--- main-dry.js
+++ main-dry.js
@@ -10,19 +10,14 @@ function initMap() {
   autocompleteLocations(map);
 }
 
-
 function autocompleteLocations(map){
-
   var orig = document.getElementById('origin');
   var dest = document.getElementById('destination');
   var autocompleteOrig = new google.maps.places.Autocomplete(orig);
   var autocompleteDest = new google.maps.places.Autocomplete(dest);
-
   autocompleteListener(autocompleteOrig,map,'SRC');
   autocompleteListener(autocompleteDest,map,'DEST');
 }
-
-
 
 function autocompleteListener(autocomplete,map,type){
   autocomplete.bindTo('bounds', map);
@@ -53,7 +48,6 @@ function autocompleteListener(autocomplete,map,type){
 
 
 function getTransitDetails(origAddr, destAddr) {
-  console.log(origAddr + ' '+destAddr);
   if(origAddr && destAddr)
   {
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -86,68 +80,48 @@ function getTransitDetails(origAddr, destAddr) {
     }
   }
 
-
-
-
-
-
-
-
-
-
   function getDetails(routes) {
-
+    var i=routes.length;
     routes.forEach(function(route) {
-      $('.js-directions-result').append('<div></div>');
-      var opt=$('.js-directions-result div:last-child');
       var legs = route.legs;
-
+      var optionHtml="";
+      var stepsHtml="";
       legs.forEach(function(leg) {
+      var steps = leg.steps;
+      optionHtml='<div class="options"><p>Route Details : </p><ul><li> Leave At : ' + leg.departure_time.text + '</li>'+
+      '<li> Reach At : ' + leg.arrival_time.text + '</li><li> Steps : ' + steps.length + '</li></ul>'+
+      '<div class="steps hidden"><ol>';
 
-        var steps = leg.steps;
-
-        opt.html( '<p>Route Details : </p><ul><li> Leave At : ' + leg.departure_time.text + '</li><li> Reach At : ' +
-        leg.arrival_time.text + '</li><li> Transfers :' + steps.length + '</li></ul>');
-
-        console.log(" Leave At : " + leg.departure_time.text);
-        console.log(" Reach At : " + leg.arrival_time.text);
-        console.log("Number of steps :" + steps.length);
-
-        opt.append('<ol></ol>');
-        var stepsList=opt.find('ol:last-child');
         steps.forEach(function(step) {
-
-          var transit = step.transit;
-          var stepOfSteps = step.steps;
-          var newStep='Instructions : ' + step.instructions+
+        var transit = step.transit;
+        var stepHtml='<li>Instructions : ' + step.instructions+
           '<ul><li>  Step Distance: ' + step.distance.text + '</li>' +
           '<li> Step Duration: ' + step.duration.text + '</li>';
-
-          if (stepOfSteps !== undefined) {
-            stepOfSteps.forEach(function(someS) {
-              console.log("Route -> Leg -> Step -> Steps-> StepOfSteps Instruction : " + someS.instructions);
-            });
-          }
-
           if (transit !== undefined) {
-            newStep+='<li>Board at : ' + transit.departure_stop.name + '</li>';
-
-
-            console.log("Board at : " + transit.departure_stop.name);
-            console.log("Get off at : " + step.transit.arrival_stop.name);
-            console.log("Boarding Time : " + transit.departure_time.text);
-            console.log("You will reach at : " + transit.arrival_time.text);
-            console.log("Headsign : " + transit.headsign);
-            console.log("Line Name  : " + transit.line.name + " & Agency : " + transit.line.agencies.map(function(item) {
-              return item.name;
-            }));
-            console.log(" Your Stop Number is  : " + transit.num_stops);
+            stepHtml+='<li>Boarding Stop : ' + transit.departure_stop.name + '</li>'+
+            '<li>Destination Stop : ' + step.transit.arrival_stop.name+ '</li>'+
+            '<li>Boarding Time : '+ transit.departure_time.text + '</li>'+
+            '<li>Reaching Time : ' + transit.arrival_time.text + '</li>'+
+            '<li>Headsign : ' + transit.headsign + '</li>'+
+            '<li>Line Name  : ' + transit.line.name + '& Agency : '+ transit.line.agencies.map(function(item) {
+              return item.name;}) + '</li><li>Stop Number :'+ transit.num_stops + '</li>';
 
           }
-          stepsList.append('<li>' + newStep + '<ul></li>');
-
+          stepHtml+='</ul></li>';
+          stepsHtml+=stepHtml;
         });
-
+      stepsHtml+='</ol>';
+      optionHtml+=stepsHtml;
       });
+      optionHtml+='</div></div>';
+      $('.js-directions-result').append(optionHtml);
     });
+    optionListeners();
   }
+
+function optionListeners(){
+  $('.options').click(function(event){
+    event.stopPropagation();
+$(this).find('.steps').toggleClass('hidden');
+  });
+}
